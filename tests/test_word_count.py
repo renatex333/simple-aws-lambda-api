92077aef624bb4416db6d37feb93d8d733aa9156
renatex333 <renato9302@hotmail.com>
2024-10-03 12:43:10 -0300

    Adding simple test.

--- tests/test_word_count.py
+++ tests/test_word_count.py
@@ -13,3 +13,12 @@ def test_simple_text_count():
 
     # Test if return is as expected
     assert my_lambda.word_count(event, None) == expected
+
+def test_no_body():
+    """Tests the word count with no body in the request."""
+    event = {}
+    expected = {
+        "created_by": "Renatex",
+        "message": "No body in the request"
+    }
+    assert my_lambda.word_count(event, None) == expected
